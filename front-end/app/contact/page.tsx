b1594b735b219d62b443559ecae69f092d081251
AGL-NguyenHaiHoangGiang <ng_hoang_giang@allgrow-labo.jp>
2026-02-06 10:31:24 +0700

    update breadcrumb

---
+++ front-end/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import Container from "@/components/layout/Container";
+import Breadcrumb from "@/components/common/Breadcrumb";
+
+export default function Contact() {
+    return (
+        <main className="">
+            
+            <Container><Breadcrumb items={[{ label: 'お問い合わせ' }]} /></Container>
+        </main>
+    )
+}
